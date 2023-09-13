936c58f9e4ef276b4be745c63af5a5508944664a
“Spencer <Spencer@bookservo.com>
2023-09-12 21:30:10 -0400

    simon game

---
+++ practice-for-ch-ruby-mancala-main/lib/board.rb
@@ -0,0 +1,49 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14){Array.new}
+    place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    cups[0..6].each do |cup|
+      4.times{cup << :stone}
+    end
+    cups[7..12].each do |cup|
+      4.times{cup << :stone}
+    end
+  end
+
+  def valid_move?(start_pos)
+    if !(start_pos).between?(1, 14)
+      raise "Invalid starting cup"
+    end
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine what #make_move returns
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+  end
+
+  def winner
+  end
+end
